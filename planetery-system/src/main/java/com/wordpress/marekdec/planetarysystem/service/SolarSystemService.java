bce555af7b7370dcb94ec3e2734a8a852e530649
marekdec <marekdec@gmail.com>
2012-01-29 23:04:53 +0100

    Created SolarSystemService

---
+++ planetery-system/src/main/java/com/wordpress/marekdec/planetarysystem/service/SolarSystemService.java
@@ -0,0 +1,9 @@
+package com.wordpress.marekdec.planetarysystem.service;
+
+import com.wordpress.marekdec.planetarysystem.domain.Planet;
+
+public interface SolarSystemService {
+
+	Planet getEarth();
+
+}
